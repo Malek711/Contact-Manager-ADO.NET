ae2cf29ec83825ee1d9d6e982c7346f9088cc203
malek <alkosimalek@gmail.com>
2026-05-22 18:01:24 +0300

    Refactor: Implement Repository Pattern and dynamic Menu System

---
+++ Contact.cs
@@ -0,0 +1,13 @@
+﻿namespace Update_Data.Folder
+{
+    public struct stcontact
+    {
+        public int ContactsID { set; get; }
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public string Email { set; get; }
+        public string Phone { set; get; }
+        public string Address { set; get; }
+        public int CountryID { set; get; }
+    }
+}
